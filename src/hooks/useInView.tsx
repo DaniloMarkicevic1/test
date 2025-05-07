c6f17ba454b69021f6ecb590ba5ca1ed360721f4
danilo.markicevic <danilo.markicevic@quantox.com>
2025-05-07 07:13:55 +0200

    fix conflixt

--- src/hooks/useInView.tsx
+++ src/hooks/useInView.tsx
@@ -3,7 +3,6 @@ import { useCallback, useRef } from 'react';
 export const useInView = (isInViewFn: () => void) => {
   const observer = useRef<IntersectionObserver | null>(null);
 
-<<<<<<< Updated upstream
   const isInViewRef = useCallback(
     (node: HTMLDivElement | null) => {
       if (observer.current) observer.current.disconnect();
@@ -18,19 +17,6 @@ export const useInView = (isInViewFn: () => void) => {
     },
     [isInViewFn],
   );
-=======
-  const isInViewRef = useCallback((node: HTMLDivElement | null) => {
-    if (observer.current) observer.current.disconnect();
-
-    observer.current = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        isInViewFn();
-      }
-    });
-
-    if (node) observer.current.observe(node);
-  }, []);
->>>>>>> Stashed changes
 
   return { isInViewRef };
 };
